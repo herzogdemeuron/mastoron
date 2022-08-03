ffde952a4f0c377c45abb8aaee4ca237348f0e8a
yschindel <55532939+yschindel@users.noreply.github.com>
2022-08-03 12:09:43 +0200

    minor fix

--- lib/mastoron/colors.py
+++ lib/mastoron/colors.py
@@ -44,7 +44,7 @@ class Color:
         Returns:
             tuple: The rgb color
         """
-        hex.lstrip('#')
+        hex = hex.lstrip('#')
         return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 
 
