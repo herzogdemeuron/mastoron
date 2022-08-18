c5422b11e70b65dc122efaa43ba2b4c95a2eceec
yschindel <55532939+yschindel@users.noreply.github.com>
2022-08-18 11:52:32 +0200

    move BarChart to ui.py

--- Mastoron.tab/Visualize.panel/Charts.pulldown/BarChart.pushbutton/BarChart_script.py
+++ Mastoron.tab/Visualize.panel/Charts.pulldown/BarChart.pushbutton/BarChart_script.py
@@ -1,4 +1,3 @@
-import clr
 import sys
 import mastoron
 import revitron
@@ -6,86 +5,11 @@ import os.path as op
 from mastoron.variables import ROUNDING_DECIMALS
 from revitron import _
 from pyrevit import forms
-from pyrevit import framework
-clr.AddReference('System')
-from System.Windows.Media import Brushes
 
-XAML_FILES_DIR = op.dirname(__file__)
+
 COUNT = 'Count'
 NUMBER_PARAMS = ['Integer', 'Double']
 DELIM = '-'
-
-
-class BarGraphWindow(mastoron.ColorSwitchWindow):
-    '''
-    Extended form to select from a list of command options.
-
-    Args:
-        context (list[str]): list of command options to choose from
-        switches (list[str]): list of on/off switches
-        message (str): window title message
-        config (dict): dictionary of config dicts for options or switches
-        recognize_access_key (bool): recognize '_' as mark of access key
-    '''
-
-    xaml_source = 'BarChartWindow.xaml'
-
-    def __init__(self, context, title, width, height, **kwargs):
-        '''
-        Initialize user input window.
-        '''
-        super(BarGraphWindow, self).__init__(
-            context, XAML_FILES_DIR, title, width, height, **kwargs, 
-            )
-
-    def resizeButtons(self, paramTotals):
-        maxValue = max(paramTotals.values())
-        scaleFactor =  (forms.DEFAULT_CMDSWITCHWND_WIDTH - 170) / maxValue
-        for button in self.button_list.Children:
-            key = button.Content
-            rawValue = int(paramTotals[key])
-            value = rawValue * scaleFactor
-            button.Width = value
-
-    @classmethod
-    def show(cls, scheme,  #pylint: disable=W0221
-            paramTotals, 
-            dataParamName,
-            title='User Input',
-            width=forms.DEFAULT_INPUTWINDOW_WIDTH,
-            height=forms.DEFAULT_INPUTWINDOW_HEIGHT, **kwargs):
-        """Show user input window.
-
-        Args:
-            context (any): window context element(s)
-            title (str): window title
-            width (int): window width
-            height (int): window height
-            **kwargs (any): other arguments to be passed to window
-        """
-        import operator
-        import collections
-        for key in scheme['data'].keys():
-            if not key in paramTotals.keys():
-                del scheme['data'][key]
-            if not key:
-                del scheme['data'][key]
-        sortedTuples = sorted(paramTotals.items(), key=operator.itemgetter(1))
-        buttonContent = collections.OrderedDict(sortedTuples).keys()
-        title = dataParamName + ' by ' + scheme['name']
-        dlg = cls(buttonContent, title, width, height, **kwargs)
-        dlg.scheme = scheme
-        dlg.colorButtons()
-        dlg.resizeButtons(paramTotals)
-        dlg.title.Content = title
-        for value in collections.OrderedDict(sortedTuples).values():
-            label = framework.Controls.Label()
-            label.Content = str(round(value, ROUNDING_DECIMALS))
-            label.Foreground = Brushes.LightGray
-            label.FontSize = 11.2
-            dlg.valueList.Children.Add(label)
-        dlg.ShowDialog()
-        return dlg.response
 
 
 scheme = mastoron.ColorScheme.getFromUser()
@@ -146,8 +70,14 @@ for element in selection:
         paramTotals[key] += float(value)
 
 revitron.Selection().set([])
-selectedParam = BarGraphWindow.show(
-    scheme, paramTotals, dataParamName, message='Search Key:')
+xamlFilesDir = op.dirname(__file__)
+xamlSource = 'BarChartWindow.xaml'
+selectedParam = mastoron.BarGraphWindow.show(scheme,
+                                            xamlFilesDir,
+                                            xamlSource,
+                                            paramTotals,
+                                            dataParamName,
+                                            message='Search Key:')
 
 if selectedParam:
     elements = revitron.Filter(selection).byStringEquals(scheme['name'], selectedParam).getElementIds()
--- lib/mastoron/ui.py
+++ lib/mastoron/ui.py
@@ -1,11 +1,13 @@
 import clr
 import os.path as op
 from mastoron.variables import SAVE
+from mastoron.variables import ROUNDING_DECIMALS
 from pyrevit import forms
 from pyrevit import framework
 from pyrevit.forms import WPFWindow
-clr.AddReference('System.Windows.forms')
+clr.AddReference('System')
 from System.Windows.Media import BrushConverter
+from System.Windows.Media import Brushes
 
 
 class ColorSwitchWindow(forms.CommandSwitchWindow):
@@ -127,3 +129,95 @@ class ColorSchemeEditor(ColorSwitchWindow):
     def save(self, sender, args):
         self.Close()
         self.response = SAVE
+
+
+class BarGraphWindow(ColorSwitchWindow):
+    '''
+    Extended form to select from a list of command options.
+
+    Args:
+        context (list[str]): list of command options to choose from
+        switches (list[str]): list of on/off switches
+        message (str): window title message
+        config (dict): dictionary of config dicts for options or switches
+        recognize_access_key (bool): recognize '_' as mark of access key
+    '''
+
+    def __init__(self,
+                context,
+                xamlFilesDir,
+                xamlSource,
+                title,
+                width,
+                height,
+                **kwargs):
+        '''
+        Initialize user input window.
+        '''
+        super(BarGraphWindow, self).__init__(context,
+                                            xamlFilesDir,
+                                            xamlSource,
+                                            title,
+                                            width,
+                                            height,
+                                            **kwargs)
+
+    def resizeButtons(self, paramTotals):
+        maxValue = max(paramTotals.values())
+        scaleFactor =  (forms.DEFAULT_CMDSWITCHWND_WIDTH - 170) / maxValue
+        for button in self.button_list.Children:
+            key = button.Content
+            rawValue = int(paramTotals[key])
+            value = rawValue * scaleFactor
+            button.Width = value
+
+    @classmethod
+    def show(cls, 
+            scheme,  #pylint: disable=W0221
+            xamlFilesDir,
+            xamlSource,
+            paramTotals, 
+            dataParamName,
+            title='User Input',
+            width=forms.DEFAULT_INPUTWINDOW_WIDTH,
+            height=forms.DEFAULT_INPUTWINDOW_HEIGHT, **kwargs):
+        """
+        Show user input window.
+
+        Args:
+            context (any): window context element(s)
+            title (str): window title
+            width (int): window width
+            height (int): window height
+            **kwargs (any): other arguments to be passed to window
+        """
+        import operator
+        import collections
+        for key in scheme['data'].keys():
+            if not key in paramTotals.keys():
+                del scheme['data'][key]
+            if not key:
+                del scheme['data'][key]
+        sortedTuples = sorted(paramTotals.items(), key=operator.itemgetter(1))
+        context = collections.OrderedDict(sortedTuples).keys()
+        title = dataParamName + ' by ' + scheme['name']
+        
+        dlg = cls(context,
+                xamlFilesDir,
+                xamlSource,
+                title,
+                width,
+                height,
+                **kwargs)
+        dlg.scheme = scheme
+        dlg.colorButtons()
+        dlg.resizeButtons(paramTotals)
+        dlg.title.Content = title
+        for value in collections.OrderedDict(sortedTuples).values():
+            label = framework.Controls.Label()
+            label.Content = str(round(value, ROUNDING_DECIMALS))
+            label.Foreground = Brushes.LightGray
+            label.FontSize = 11.2
+            dlg.valueList.Children.Add(label)
+        dlg.ShowDialog()
+        return dlg.response
